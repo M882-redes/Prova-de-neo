7e042254c358f6b67a2dc1a7011fe5dd347653ac
Maria Vitória <Maria Vitria@LAPTOP-NH2S5V99>
2019-12-02 15:00:49 -0300

    Codigo funcionario criação da classe conexão, metodo logar

--- src/DAO/FuncionarioDAO.java
+++ src/DAO/FuncionarioDAO.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package DAO;
+import java.sql.*;
+import Modelo.Funcionario;
 
-/**
- *
- * @author Maria Vitória
- */
-public class FuncionarioDAO {
+public class FuncionarioDAO  extends ExecuteSQL{
+    
+    public FuncionarioDAO(Connection con) {
+        super(con);
+    }
+    public boolean Logar(String login, String senha) {
+        boolean finalResult = false;
+        try{ 
+            String consulta = "select login, senha from funcionario "
+                    + "where login = '"+ login +"admin' and senha = '"+ senha+"admin' ";
+            PreparedStatement ps = getCon().prepareStatement(consulta);
+            ResultSet rs  = ps.executeQuery();
+            
+            
+           if(rs != null) {
+               while (rs.next()) {
+                   Funcionario a = new Funcionario();
+                   a.setLogin(rs.getString(1));
+                   a.getSenha(rs.getString(2));
+                   finalResult = true;
+                  
+               }
+           }
+                   
+        } catch (SQLException ex) {
+            ex.getMessage();
+            
+        }
+         return finalResult;
+                }
+    
     
 }
